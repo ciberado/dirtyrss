89ee1dbd5d8893a7806fb8df26200abf81d501c8
Javi Moreno <email@javier-moreno.com>
2022-01-18 20:30:01 +0100

    modified regex for extracting id of the show

--- src/IVoox.ts
+++ src/IVoox.ts
@@ -33,7 +33,7 @@ export class IVoox {
 
         const audioUrlTempl = "https://www.ivoox.com/listenembeded_mn_12345678_1.mp3?source=EMBEDEDHTML5";
 
-        const id = (url.match(/\d{4,8}/g))![0];
+        const id = (url.match(/\d{6,12}/g))![0];
         const audioRealUrl = audioUrlTempl.replace('12345678', id);
 
         const description = $('.description').text().trim();
