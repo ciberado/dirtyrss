abbebf56ef45363877062025483483cb05d95c70
Javi Moreno <email@javier-moreno.com>
2022-04-09 16:30:50 +0200

    New api draft

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,6 @@ import { TwitchChannel } from './TwitchChannel.js';
 
 
 await TwitchChannel.ensureTwitchDl();
-const tc = new TwitchChannel('mossegalapoma');
-await tc.fetchChannelData();
-
-if (0===0) process.exit(0);
 
 async function generateFeed(programName: string) : Promise<string> {
     const programUrl = await IVoox.search(programName);
@@ -35,6 +31,39 @@ app.get('/', async (req, reply) => {
         reply.code(404).type('text/html').send(`Podcast ${params.podcast} not found (${e}).`);
     }
 });
+
+interface HttpRequest {
+    params : {
+        showId : string;
+    }
+    query : {
+        podcast : string;
+    }
+}
+
+app.get('/twitch/:showId', async (req, reply) => {
+    const request = req as HttpRequest;
+    try {
+        const tc = new TwitchChannel(request.params.showId);
+        const xmlFeed = await tc.generateFeed();
+        reply.send(xmlFeed);            
+    } catch (err) {
+        console.warn(err);
+        reply.code(404).type('text/html').send(`Podcast ${request.params.showId} not found (${err}).`);
+    }
+});
+
+app.get('/ivoox/:showId', async (req, reply) => {
+    const request = req as HttpRequest;
+    try {    
+        const xmlFeed = await generateFeed(request.params.showId);
+        reply.send(xmlFeed);    
+    } catch (err) {
+        console.warn(err);
+        reply.code(404).type('text/html').send(`Podcast ${request.params.showId} not found (${err}).`);
+    }
+});
+
 app.listen(3000, '0.0.0.0', function (err, address) {
-    console.log(`Server started.`);
+    console.log(`Server started at port 3000.`);
 });
